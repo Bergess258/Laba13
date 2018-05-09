eac54c345cda4d50edf2af71b354b22a330a6c4a
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-05-09 12:50:11 +0500

    hvj

---
+++ Laba13/Laba13/List.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba13
+{
+    class List<PlacesV> : IEnumerable<PlacesV>, ICloneable, ICollection<PlacesV>, IComparer<PlacesV>
+    {
+        private int count = 0;
+        private PlacesV[] mas = new PlacesV[0];
+        public List()
+        {
+
+        }
+        public List(int cap)
+        {
+            mas = new PlacesV[cap];
+        }
+        public List(PlacesV[] mas)
+        {
+            this.mas = mas;
+        }
+
+        public PlacesV this[int index]
+        {
+            get { return mas[index]; }
+            set { mas[index] = value; }
+        }
+        static void RandAdd(List<PlacesV> Main,int CountAdding)
+        {
+            do
+            {
+                Random rand = new Random();
+                string Name = "";
+                int c = rand.Next(4);
+                if (c == 0)
+                {
+                    Region temp;
+                    do
+                    {
+                        Name = RandomNameRegion(rand);
+                        temp = new Region(Name, rand.Next(0, 10000000), rand.Next(0, 20));
+                    } while (Main.Contains(temp));
+                    Add(temp);
+                }
+                else
+                if (c == 1)
+                {
+                    City temp;
+                    do
+                    {
+                        Name = RandomCity(rand);
+                        temp = new City(Name, rand.Next(0, 900000));
+                    } while (Contains((PlacesV)temp));
+                    Places.Add(temp);
+                }
+                else
+                if (c == 2)
+                {
+                    Megapolis temp;
+                    do
+                    {
+                        Name = RandomMegapolis(rand);
+                        temp = new Megapolis(Name, rand.Next(0, 20));
+                    } while (Contains(temp));
+                    Places.Add(temp);
+                }
+                else
+                {
+                    Adres temp;
+                    do
+                    {
+                        Name = RandomAdres(rand);
+                        temp = new Adres(Name);
+                    } while (Contains(temp));
+                    Add(temp);
+                }
+            } while (CountAdding-- != 0);
+        }
+        public void Add(PlacesV item)
+        {
+            if (count == mas.Length)
+            {
+                PlacesV[] Temp = new PlacesV[mas.Length + 1];
+                mas.CopyTo(Temp, 0);
+                Temp[Temp.Length - 1] = item;
+                mas = Temp;
+            }
+            else
+                mas[count] = item;
+            count++;
+        }
+        public void Clear()
+        {
+            mas = new PlacesV[0];
+        }
+        public void CopyTo(PlacesV[] array, int index)
+        {
+            if (index > 0 && index <= mas.Length)
+            {
+                PlacesV[] Temp = new PlacesV[mas.Length - index + 1];
+                int c = 0;
+                for (int i = index - 1; i < mas.Length; i++)
+                    Temp[c++] = mas[i];
+                array = Temp;
+            }
+            else
+                throw new IndexOutOfRangeException();
+        }
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+        public bool Remove(PlacesV item)
+        {
+            if (mas.Contains(item))
+            {
+                PlacesV[] Temp = new PlacesV[mas.Length - 1];
+                int c = 0, c1 = -1;
+                foreach (PlacesV temp in mas)
+                {
+                    c1++;
+                    if (!temp.Equals(item)) Temp[c++] = mas[c1];
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+        public void Show()
+        {
+            foreach (PlacesV temp in mas)
+            {
+                Console.WriteLine(temp.ToString());
+            }
+        }
+        public bool Contains(PlacesV Search)
+        {
+            foreach (PlacesV temp in mas)
+            {
+                if (temp.ToString() == Search.ToString()) return true;
+            }
+            return false;
+        }
+        public IEnumerator<PlacesV> GetEnumerator()
+        {
+            return ((IEnumerable<PlacesV>)mas).GetEnumerator();//вапрыва
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (this as IEnumerable<PlacesV>).GetEnumerator();
+        }
+
+        object ICloneable.Clone()
+        {
+            return this;
+        }
+        public void Sort()
+        {
+            Array.Sort(mas);
+        }
+
+        public int Compare(PlacesV x, PlacesV y)
+        {
+            if (x.ToString().Length == y.ToString().Length) return 0;
+            if (x.ToString().Length > y.ToString().Length) return 1;
+            return -1;
+        }
+    }
+}
---
+++ Laba13/Laba13/PlacesV.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba13
+{
+    class PlacesV : IComparable
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public PlacesV()
+        {
+            Name = "";
+        }
+        public PlacesV(string name)
+        {
+            Name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj.ToString() == this.ToString()) return 0;
+            if (obj.ToString().Length > this.ToString().Length) return 1;
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+    class Region : PlacesV //Кол-во мужчин во всех регионах
+    {
+        int numberMans = 0;
+        int numberCities = 0;
+        public int NumberCities
+        {
+            get { return numberCities; }
+            set { numberCities = value; }
+        }
+        public int NumberMans
+        {
+            get { return numberMans; }
+            set
+            {
+                if (value < 0) Console.WriteLine("Мужчин не может быть меньше 0");
+                else numberMans = value;
+            }
+        }
+        public Region() : base()
+        {
+
+        }
+        public Region(string name) : base(name)
+        {
+
+        }
+        public Region(string name, int mans) : base(name)
+        {
+            NumberMans = mans;
+        }
+        public Region(string name, int mans, int cit) : base(name)
+        {
+            NumberMans = mans;
+            numberCities = cit;
+        }
+        public override string ToString()
+        {
+            return Name + " Область";
+        }
+    }
+
+    class City : PlacesV // кол-во горожан во всех регионах 
+    {
+        int citizens = 0;
+        public int Citizens
+        {
+            get { return citizens; }
+            set
+            {
+                if (value < 0) Console.WriteLine("Горожан не может быть меньше 0");
+                else citizens = value;
+            }
+        }
+        public City() : base()
+        {
+
+        }
+        public City(string name) : base(name)
+        {
+        }
+        public City(string name, int Countcitizens) : base(name)
+        {
+            Citizens = Countcitizens;
+        }
+        public override string ToString()
+        {
+            return "Город " + Name;
+        }
+    }
+
+    class Megapolis : PlacesV
+    {
+        private int countFabriks;
+        public int CounFabriks
+        {
+            get { return countFabriks; }
+            set { countFabriks = value; }
+        }
+        public Megapolis() : base()
+        {
+
+        }
+        public Megapolis(string name) : base(name)
+        {
+        }
+        public Megapolis(string name, int t) : base(name)
+        {
+            countFabriks = t;
+        }
+        public override string ToString()
+        {
+            return "Мегаполис " + Name;
+        }
+    }
+
+    class Adres : PlacesV
+    {
+        int index;
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        public Adres() : base()
+        {
+
+        }
+        public Adres(string name) : base(name)
+        {
+
+        }
+        public Adres(string name, int index1) : base(name)
+        {
+            index = index1;
+        }
+        public PlacesV BasePlace
+        {
+            get { return new PlacesV("Неизвестный город"); }
+        }
+        public override string ToString()
+        {
+            return "Адрес: " + Name;
+        }
+    }
+}
